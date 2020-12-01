4ee17b7c08be6cf65f02bc7026fa1e7d79ec7533
McKay <mckay@lucidchart.com>
2020-12-01 15:10:16 -0700

    Add Tampermonkey Scripts

---
+++ Tampermonkey Scripts/Spotify Resume on Pause.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Spotify-uBlock-Origin-Ad_Pause-Fix
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Fixes spotify getting stuck on "Advertisement" when ublock origin is in use
+// @author       anonymous
+// @match        https://*.spotify.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function () {
+console.log("Checking if active state is on an AD.");
+
+        var is_stuck = document.title.startsWith("Advertisement");
+
+        if (is_stuck) {
+console.log("AD Detected. Attempting to press the Pause button to bypass the add and resume playing.");
+
+            var buttons = document.getElementsByClassName("player-controls__buttons")[0];
+            var b = buttons.childNodes[2].childNodes[0];
+            var playPauseButton = document.querySelector("button[title='Play'], button[title='Pause']");
+
+            if (b != null) {
+                playPauseButton.click();
+console.log("Play/Pause Button Clicked.");
+
+            }
+
+        }
+    }, 1000);
+
+})();
