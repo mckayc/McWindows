bbe40874f918790ed85a19496df655058c2fdccf
McKay <mckay@lucidchart.com>
2020-12-08 06:51:57 -0700

    Added option for continuous play

--- Tampermonkey Scripts/Spotify Resume on Pause.js
+++ Tampermonkey Scripts/Spotify Resume on Pause.js
@@ -12,20 +12,32 @@
     'use strict';
     setInterval(function () {
 
-        var nowplaying = document.getElementsByClassName("now-playing")[0].getAttribute("aria-label"); // Find out the currently playing song or ad in the "now playing" section of Spotify
-        console.log(`Now Playing = ${nowplaying}`);
-        var playing_ad = document.title.startsWith("Advertisement") || nowplaying.includes("Advertisement") || nowplaying === ""; // Find out the current state of Spotify; what is listed in the document/tab
-        console.log(`Document Title = ${document.title}`);
+        // If you want Spotify to play continuously and never pause, set to "y", otherwise set to anything else
+        // Note; this will force play; even if you manually pause it. Default set to "n"
+        var continuousPlay = "n"
 
+        // Find out the currently playing song or ad in the "now playing" section of Spotify
+        var nowplaying = document.getElementsByClassName("now-playing")[0].getAttribute("aria-label"); 
+        console.log(`Now Playing = ${nowplaying}`);
+
+        // Find out the current state of Spotify; what is listed in the document/tab - Set to true if on Advertisement
+        var playing_ad = document.title.startsWith("Advertisement") || nowplaying.startsWith("Advertisement"); 
+        console.log(`Document Title = ${document.title}`);
         console.log(`'Playing_Ad' state = ${playing_ad}`); // states true/false for if the state is "Playing_Ad"
 
+        // Find out if Spotify is in a Pause or Play state to enable continuous play
+        var paused = document.getElementsByClassName("_82ba3fb528bb730b297a91f46acd37a3-scss")[0].getAttribute("aria-label");
+        console.log(`Paused state = ${paused}`)
+        console.log(`Continuous Play is set to = ${continuousPlay}`)
+
+        // Display a time stamp for statistical purposes
         var date, formattedTime;
          date = new Date();
          formattedTime = date.toISOString();
         console.log(formattedTime);
 
-        if (playing_ad) {
-            console.log("State detected as being paused after ad. Attempting to press the Pause button to resume playing media.");
+        if ((playing_ad) || (paused === "Play" && continuousPlay === "y")) {
+            console.log("State detected as being paused. Attempting to press the Pause button to resume playing media.");
 
             var buttons = document.getElementsByClassName("player-controls__buttons")[0];
             var b = buttons.childNodes[2].childNodes[0];
