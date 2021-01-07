f824536b21b1035a367abfb4b7f13f1844850394
McKay <mckay@lucidchart.com>
2021-01-07 11:13:26 -0700

    Progress

--- PowershellScripts/RandomTrackGenerator.ps1
+++ PowershellScripts/RandomTrackGenerator.ps1
@@ -10,14 +10,22 @@ $trackList = Import-Csv -Path .\trackList.csv # Import Tracklist from csv
 $iterations = 50000 # Specify how many track plays you want to simulate
 $count = 0 # Variable to use in the while loop to generate new list
 $newList = @() # Initiate the array
+$initialTime = Get-Date 
 
 while($count -lt $iterations) # Only do as many tracks as specified
 {
     $randomPlay = $trackList | Get-Random -Count 1 # Pull single random track from playlist
     $newList += $randomPlay # Append track to new list
-    $count++
-    $percentComplete = ($count / $iterations)*100
-    Write-Progress "Step 1 of 2): Generate list - $count of $iterations - Percent complete = $percentComplete%"
+    $count++ 
+
+    $updatedTime = Get-Date 
+    $elapsedTime = New-TimeSpan -Start $initialTime -End $updatedTime
+    $percentComplete = ($count / $iterations)*100 # Math to calculate percent complete
+
+    $percentCompleteCropped = "{0:n2}" -f $percentComplete # Format to 2 decimal places. Force 0
+    $elapsedTimeFormatted = $elapsedTime 
+
+    Write-Progress "Step 1 of 2): Generate list - $count of $iterations - Percent complete = $percentCompleteCropped% - Elapsed time = $elapsedTimeFormatted"
     $randomPlay | export-csv –append –Path "simulatedTrackList.csv" -NoTypeInformation
 }
 
