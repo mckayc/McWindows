cc3a32da8859b577ddad6ab71d06921755588c8f
mckayc <mckayjunk@gmail.com>
2022-09-26 14:14:23 -0600

    typo fix

--- McWindowsProvisioningScript.ps1
+++ McWindowsProvisioningScript.ps1
@@ -1,6 +1,9 @@
 # User Specified Variables
 $repository = "https://raw.githubusercontent.com/mckayc/McWindows/master/McChocolateyList.txt"
 
+# Questions for user setup
+$displaySummary = Read-Host "Would you like to display the Summary of each package? (this takes longer to load package names) y / (blank)"
+
 # Pre-set variables - do not change
 $tempFolder = $env:TEMP
 $txtFile = "$tempFolder\repository.txt"
@@ -12,18 +15,27 @@ Invoke-WebRequest -Uri $repository -OutFile $txtFile
 $imported = Get-Content -raw $txtFile | ConvertFrom-StringData
 $packageList = $imported
 
-
 # Messaging for basic instructions
 Write-Host "If you would like to install the listed package, please enter 'y'; otherwise leave blank."
+Write-Output " "
+Write-Output " "
 
-$packageList = $packageList.GetEnumerator() | sort -Property Value 
+# Sort by package then by category
+$packageList = $packageList.GetEnumerator() | sort -Property Value, Name  
 
 foreach ($package in $packageList) {
 
-    # $package
-    # choco info $package | Select-String -Pattern 'Summary'
+    $packageName = $package.name
+    $packageCategory = $package.value
+
+    if ($displaySummary -eq "y")
+    {
+        Write-Output "`r"
+        choco info $package.name | Select-String -Pattern 'Title'
+        choco info $package.name | Select-String -Pattern 'Summary'
+    }
     
-    $confimation = Read-Host $package.value "-" $package.name
+    $confimation = Read-Host "$packageCategory - $packageName"
     if ($confimation -eq "y")
     {
         $acceptedPackages += $package
@@ -31,6 +43,6 @@ foreach ($package in $packageList) {
 }
  
 foreach ($acceptedPackage in $acceptedPackages) {
-    Write-Output "Installing the package $acceptedPackage"
-    choco install $acceptedPackage -y
+    Write-Output "Installing the package " $acceptedPackage.name
+    choco install $acceptedPackage.name -y
 }
