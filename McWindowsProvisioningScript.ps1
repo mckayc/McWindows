1b3095c5709b60575cc73d0aaeec46c7bea8f3ad
mckayc <mckayjunk@gmail.com>
2022-09-26 13:04:50 -0600

    Change list

--- McWindowsProvisioningScript.ps1
+++ McWindowsProvisioningScript.ps1
@@ -1,16 +1,30 @@
+# User Specified Variables
+$repository = "https://raw.githubusercontent.com/mckayc/McWindows/master/McChocolateyList.txt"
+
+# Pre-set variables - do not change
+$tempFolder = $env:TEMP
+$txtFile = "$tempFolder\repository.txt"
+$acceptedPackages = @()
+
+# Download file and set as hashtable
+Invoke-WebRequest -Uri $repository -OutFile $txtFile
+
+$imported = Get-Content -raw $txtFile | ConvertFrom-StringData
+$packageList = $imported
+
 Write-Host "If you would like to install the listed package, please enter 'y'; otherwise leave blank."
 
-$acceptedPackages = @()
-$packageList = @{
-    googlechrome = 'Internet'
-    sharex  = 'Tools'
-    neverball = 'Games'
-    wireguard = 'Internet'
-    supertuxkart = 'Games'
-    qbittorrent = 'Internet'
-    openvpn = 'Internet'
-    freecad = 'Graphics'
-}
+
+# $packageList = @{
+#     googlechrome = 'Internet'
+#     sharex  = 'Tools'
+#     neverball = 'Games'
+#     wireguard = 'Internet'
+#     supertuxkart = 'Games'
+#     qbittorrent = 'Internet'
+#     openvpn = 'Internet'
+#     freecad = 'Graphics'
+# }
 $packageList = $packageList.GetEnumerator() | sort -Property Value 
 
 foreach ($package in $packageList) {
@@ -24,7 +38,6 @@ foreach ($package in $packageList) {
         $acceptedPackages += $package
     }
 }
-
  
 foreach ($acceptedPackage in $acceptedPackages) {
     Write-Output "Installing the package $acceptedPackage"
