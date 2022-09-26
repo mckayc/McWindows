0b149dd383366b4cfac723cff5e198e2b4ea05eb
mckayc <mckayjunk@gmail.com>
2022-09-26 16:40:06 -0600

    More

--- McWindowsProvisioningScript.ps1
+++ McWindowsProvisioningScript.ps1
@@ -1,3 +1,21 @@
+#Instructions - Paste the following into IE: 
+#http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/mckayc/McWindows/master/McWindowsProvisioningScript.ps1
+
+
+Set-WindowsExplorerOptions -EnableShowProtectedOSFiles -EnableShowFileExtensions
+Enable-RemoteDesktop
+Set-StartScreenOptions -EnableBootToDesktop
+
+# Install Core
+cinst chocolatey -y
+cinst chocolateygui -y
+
+
+
+
+
+
+
 # User Specified Variables
 $repository = "https://raw.githubusercontent.com/mckayc/McWindows/master/McChocolateyList.txt"
 
