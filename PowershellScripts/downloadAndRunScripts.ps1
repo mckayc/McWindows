653c3dc60dcab49240e5e8ec54505339ee33fed6
mckayc <mckayjunk@gmail.com>
2022-11-03 14:25:44 -0600

    Almost working with test URL

--- PowershellScripts/downloadAndRunScripts.ps1
+++ PowershellScripts/downloadAndRunScripts.ps1
@@ -1,3 +1,50 @@
 # This script will download the URL you specify, extract the file, then run the files you request
 
+# Variables that require input
+#$packageURL = "Add URL Here"
+$packageURL = "https://github.com/lixuy/LTSC-Add-MicrosoftStore/archive/2019.zip"
+
+
+# Pre-set variables (optional to change)
+$fileLocation = "C:\Windows\Temp"
+$zipName = "scripts.zip"
+$zipFolder = "scripts"
+$outpath = "$fileLocation/$zipName"
+
+# Create Hash Table
+$scriptExtensions=
+@(
+    'cmd'
+    'ps1'
+)
+
+# Download Zip folder to temp location
+Invoke-WebRequest -Uri $packageURL -OutFile $outpath
+
+# Extract Zip folder
+Expand-Archive -Path "$fileLocation/$zipName" -DestinationPath "$fileLocation/$zipFolder"
+
+
+# Search for all cmd files and run the script
+$n = 1
+foreach($scriptExtension in $scriptExtensions) 
+{
+    # Search for all cmd files and run the script
+        foreach($script in Get-ChildItem -Path "$fileLocation/$zipFolder" -Recurse *.$scriptExtension) 
+    {
+        $script.FullName
+        Write-Host "Installing script #$n"
+        $n++
+    }
+}
+
+
+
+
+
+
+
+
+# Run Scripts
+
 
