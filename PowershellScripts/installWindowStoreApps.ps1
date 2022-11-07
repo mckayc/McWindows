4053a4e6cb39d34a17a5c79cb943e069280a98ce
mckayc <mckayjunk@gmail.com>
2022-11-07 16:06:58 -0700

    add show info as a test

--- PowershellScripts/installWindowStoreApps.ps1
+++ PowershellScripts/installWindowStoreApps.ps1
@@ -1,3 +1,6 @@
+# Winget test - Show the info so the scripts below don't fail.
+winget --info
+
 # Install Microsoft Photos
 winget install -e --silent --accept-source-agreements --accept-package-agreements --id 9WZDNCRFJBH4
 # Install Windows Calculator
