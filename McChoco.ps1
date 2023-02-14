cb4fbeb69ccd96de5bf2285ac489c437c999a1dd
mckayc <mckayjunk@gmail.com>
2023-02-14 09:59:46 -0700

    Update McChoco.ps1

--- McChoco.ps1
+++ McChoco.ps1
@@ -1,5 +1,11 @@
 #Instructions - Paste the following into IE: 
 #http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/mckayc/McWindows/master/McChoco.ps1
 
+#Install Boxstarter
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
+
 # Install Core
+cinst chocolatey -y
+
+#Install Other
 cinst geekbench -y
