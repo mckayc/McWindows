0a0f61660ed2a12cdc4dd0dead1c0402268e950d
McKay <mckay@lucidchart.com>
2023-02-28 21:56:06 -0700

    Test

--- McChoco.ps1
+++ McChoco.ps1
@@ -2,7 +2,5 @@
 # http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/mckayc/McWindows/master/McChoco.ps1
 
 # Install Boxstarter
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
+https://boxstarter.org/package/geekbench,googlechrome
 
-cinst chocolatey -y
-cinst geekbench -y
